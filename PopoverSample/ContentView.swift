0e388e849397b3dad3c6cb0469acb7d86540bd03
Martin <yuangang.sheng@itc-engineering.com>
2021-01-27 14:18:13 +0100

    Add the popovers.

--- PopoverSample/ContentView.swift
+++ PopoverSample/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showPopover1 = false
+    @State private var showPopover2 = false
+    @State private var sort: Int = 0
+
+    @State private var selectedFrameworkIndex = 0
+    let menuStrings = ["menu1", "menu2", "menu3", "menu4", "menu5", "menu6"]
+    var frameworks = ["UIKit", "Core Data", "CloudKit", "SwiftUI"]
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+
+            Button("Show Popover1") {
+                self.showPopover1.toggle()
+            }.popover(
+                isPresented: self.$showPopover1,
+                attachmentAnchor: .point(UnitPoint.top), arrowEdge: .top, content: { Text("Test11111") }
+            )
+
+            Button("Show Popover2") {
+                self.showPopover2.toggle()
+            }
+            .popover(isPresented: self.$showPopover2) {
+                Text("Test2222222")
+            }
+        }
     }
 }
 
